1662dcd979acbfe57dd527c1949f81a592e3283e
alexacw <alex_acw@outlook.com>
2019-03-22 23:19:12 +0800

    voltage and current feedback

--- Inc/sampling.h
+++ Inc/sampling.h
@@ -1,5 +1,3 @@
 
-#define CURRENT_SAMPLE_AMOUNT 50
-extern float currentAvg;
 
 void SamplingStart(void);
--- Src/sampling.c
+++ Src/sampling.c
@@ -18,18 +18,23 @@
 #include "sampling.h"
 #include "stm32f3xx_hal.h"
 
-uint16_t dacValue = 1125;
+#define CURRENT_SAMPLE_AMOUNT 50
+#define VOLTAGE_SAMPLE_AMOUNT 50
+const uint16_t dacValue = 1125;
 uint32_t currentSamples[CURRENT_SAMPLE_AMOUNT];
-int32_t currentSum = 0;
+uint32_t voltageSamples[VOLTAGE_SAMPLE_AMOUNT];
 float currentAvg = 0;
+float voltage = 0;
 
 void SamplingCANTransmitThd(void const *argument)
 {
     CAN_TxHeaderTypeDef pHeader;
-    static volatile tempCurrent;
+    static volatile uint8_t data[8];
     while (1)
     {
-        tempCurrent = currentAvg;
+        voltage = HAL_SDADC_GetValue(&hsdadc1) / 357.25;
+        *(float *)data = currentAvg;
+        *(float *)&data[4] = voltage;
         pHeader.StdId = 0x301;
         // pHeader.ExtId =
         pHeader.IDE = CAN_ID_STD;
@@ -38,7 +43,7 @@ void SamplingCANTransmitThd(void const *argument)
         pHeader.TransmitGlobalTime = DISABLE;
 
         uint32_t mailbox;
-        HAL_CAN_AddTxMessage(&hcan, &pHeader, &tempCurrent, &mailbox);
+        HAL_CAN_AddTxMessage(&hcan, &pHeader, data, &mailbox);
         osDelay(5);
     }
 }
@@ -47,9 +52,15 @@ void SamplingStart(void)
 {
     HAL_DAC_Start(&hdac1, DAC_CHANNEL_1);
     HAL_DAC_SetValue(&hdac1, DAC_CHANNEL_1, DAC_ALIGN_12B_R, (uint32_t)dacValue);
+
     HAL_SDADC_CalibrationStart(&hsdadc3, SDADC_CALIBRATION_SEQ_1);
     HAL_SDADC_PollForCalibEvent(&hsdadc3, HAL_MAX_DELAY);
+
+    HAL_SDADC_CalibrationStart(&hsdadc1, SDADC_CALIBRATION_SEQ_1);
+    HAL_SDADC_PollForCalibEvent(&hsdadc1, HAL_MAX_DELAY);
+
     HAL_SDADC_Start_DMA(&hsdadc3, currentSamples, CURRENT_SAMPLE_AMOUNT);
+    HAL_SDADC_Start_DMA(&hsdadc1, voltageSamples, VOLTAGE_SAMPLE_AMOUNT);
 
     //start CAN current transmission thread
     osThreadDef(samplingCANTx, SamplingCANTransmitThd, osPriorityNormal, 0, 128);
@@ -58,6 +69,8 @@ void SamplingStart(void)
 
 void HAL_SDADC_ConvCpltCallback(SDADC_HandleTypeDef *hsdadc)
 {
+    static int32_t currentSum = 0;
+    static int32_t voltageSum = 0;
     if (hsdadc == &hsdadc3)
     { //current
         currentSum = 0;
@@ -66,5 +79,11 @@ void HAL_SDADC_ConvCpltCallback(SDADC_HandleTypeDef *hsdadc)
             currentSum += (int16_t)(currentSamples[i] & 0xFFFF);
         }
         currentAvg = (-currentSum * 1.0 / CURRENT_SAMPLE_AMOUNT + 25.0) / 3885.5;
+        return;
+    }
+    if (hsdadc == &hsdadc1)
+    { //voltage
+        voltageSamples[0]++;
+        return;
     }
 }
